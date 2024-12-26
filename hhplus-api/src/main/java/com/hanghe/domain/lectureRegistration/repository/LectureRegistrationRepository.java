d8e1efad276189751631023bf240eb2cad54386c
jd <jd0922@naver.com>
2024-12-27 08:51:28 +0900

    service 구현

---
+++ hhplus-api/src/main/java/com/hanghe/domain/lectureRegistration/repository/LectureRegistrationRepository.java
@@ -0,0 +1,30 @@
+package com.hanghe.domain.lectureRegistration.repository;
+
+import com.hanghe.domain.lecture.entity.Lecture;
+import com.hanghe.domain.lectureRegistration.entity.LectureRegistration;
+import com.hanghe.domain.lectureRegistration.state.LectureRegistrationState;
+import com.hanghe.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface LectureRegistrationRepository extends JpaRepository<LectureRegistration,Long> {
+
+    /** 강의 신청 조회*/
+    LectureRegistration findByUserAndLectureAndLrState(User user, Lecture lecture, LectureRegistrationState state);
+
+    /** 유저 신청 강의 목록 조회*/
+    List<LectureRegistration> findAllByUserAndLrState(User user, LectureRegistrationState state);
+
+    /** 강의 등록인원 수 조회*/
+    int countByLectureAndLrState(Lecture lecture, LectureRegistrationState state);
+
+    /** 강의 신청 max sequence */
+    @Query("SELECT COALESCE(MAX(r.lrSequence), 0) FROM LectureRegistration r WHERE r.lecture = :lecture")
+    int findMaxSequenceByLecture(@Param("lecture") Lecture lecture);
+
+    /** 강의 신청 초과 데이터 조회 */
+    List<LectureRegistration> findAllByLectureAndLrStateAndLrSequenceGreaterThan(Lecture lecture, LectureRegistrationState state, int limit);
+}
---
+++ hhplus-api/src/main/java/com/hanghe/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.hanghe.domain.user.repository;
+
+import com.hanghe.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
